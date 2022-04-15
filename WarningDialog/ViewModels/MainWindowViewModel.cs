9cbfc104e5593f54b7536c5835121d700a55fe4a
pavelvasilev8500 <pavelvasilev@live.com>
2022-04-15 09:43:16 +0300

    Add autoclose warning window when laptop charging.

--- WarningDialog/ViewModels/MainWindowViewModel.cs
+++ WarningDialog/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,10 @@
 using Prism.Events;
 using Prism.Mvvm;
 using ResourcesLibrary.Resources.Languages.Classes;
+using System;
 using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace WarningDialog.ViewModels
 {
@@ -14,11 +17,34 @@ namespace WarningDialog.ViewModels
         {
             _ea = ea;
             _ea.GetEvent<SendLanguageEvent>().Subscribe(MessageReceived);
+            ThreadController();
         }
 
         private void MessageReceived(CultureInfo language)
         {
             Languages.Language = language;
+        }
+
+        private void ThreadController()
+        {
+            Thread batteryCheck = new Thread(() =>
+            {
+                BatteryCheck();
+            });
+            batteryCheck.Name = "BatteryCheckThread";
+            batteryCheck.Start();
+        }
+
+        private void BatteryCheck()
+        {
+            do
+            {
+                if(SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.Charging)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            while (true);
         }
     }
 }
